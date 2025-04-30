3359f42335466856a2361ccb3b3fa7d29c5291f4
nwpm <fwxgkt@gmail.com>
2025-04-30 18:53:24 +0300

    Delete c_count_symbols

--- include/c_string.h
+++ include/c_string.h
@@ -3,28 +3,31 @@
 
 int c_strlen(const char s[]);
 int c_strcmp(const char first[], const char second[]);
-int c_count_symbols(const char s[]);
 int c_remove_digits(char s[]);
 int c_is_string_of_digits(const char s[]);
-int c_delete_duplicates(char s[]);
 int c_is_palindrom(const char s[]);
 int c_count_words(const char s[]);
 int c_is_char_in_string(const char s[], const char chr);
 int c_first_unique_char(const char s[]);
 
-int c_is_symbol(const char c);
+int c_is_punct_char(const char c);
 int c_is_letter(const char c);
 int c_is_digit(const char c);
 int c_is_space(const char c);
 int c_is_tab(const char c);
 int c_is_empty_string(const char s[]);
 
+void c_delete_duplicates(char s[]);
 void c_delete_spaces(char s[]);
 void c_strcpy(char to[], const char from[]);
 void c_to_lower(char s[]);
 void c_to_upper(char s[]);
 void c_strcat(char to[], char from[]);
 void c_change_char_to(char s[], const char from_char, const char to_char);
+void c_invert_symbols(char s[]);
+void c_trim(char s[]);
+void c_reverse(char s[]);
+void c_delete_punctuation(char s[]);
 
 /////
 
@@ -35,10 +38,5 @@ void c_reverse_word_order(char s[], const char delim);
 void c_sort_chars(char s[]);
 void c_sort_words(char s[]);
 void c_delete_word_duplicate(char s[]);
-
-void c_invert_symbols(char s[]);
-void c_delete_punctuation(char s[]);
-void c_reverse(char s[]);
-void c_trim(char s[]);
 
 #endif // !C_STRING_H
