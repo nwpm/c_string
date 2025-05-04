b3e1c70bb6b43c88325410b47ee356a8abe5cae5
nwpm <fwxgkt@gmail.com>
2025-05-04 19:43:18 +0300

    Added declarations for c_is_anagrams, c_str_replace_first, c_str_replaceall and c_str_replace_n

--- include/c_string.h
+++ include/c_string.h
@@ -9,6 +9,8 @@ int c_is_palindrom(const char s[]);
 int c_count_words(const char s[]);
 int c_is_char_in_string(const char s[], const char chr);
 int c_first_unique_char(const char s[]);
+int c_strstr(const char *s, const char *substr);
+int c_num_substr(const char *s, const char *substr);
 
 int c_is_punct_char(const char c);
 int c_is_letter(const char c);
@@ -31,17 +33,21 @@ void c_delete_punctuation(char s[]);
 void c_sort_chars(char s[]);
 void c_detab(char s[], const int space_for_tab);
 void c_entab(char s[], const int space_for_tab);
-void c_insert_substr_from(char *to, const char *substr, int *insert_from);
+void c_insert_str_from(char *to, const char *substr, const int insert_from);
 void c_reverse_word_order(char s[], const char delim);
 void c_delete_word_duplicate(char *s);
 void c_strtok(char *s, const int cols, char (*result)[cols], const char delim);
 void c_sort_words(char *s);
+void c_str_replace_first(char *s, const char *substr_old,
+                         const char *substr_new);
+void c_str_replace_all(char *s, const char *substr_old, const char *substr_new);
+void c_str_replace_n(char *s, const char *substr_old, const char *substr_new,
+                     const int n);
+int c_is_anagrams(const char *s1, const char *s2);
 
 /////
 
-int c_strstr(char *s);
-int c_num_substr(char *s, const char *substr);
-void c_atoi(char *s);
+int c_atoi(const char *s);
 void c_itoa(int num);
 
 #endif // !C_STRING_H
