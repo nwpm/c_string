621b0cb1911ed756a9b5e02f922e0f0692130182
nwpm <fwxgkt@gmail.com>
2025-05-06 16:15:55 +0300

    separate file for consts and typedef

---
+++ src/internal/_c_common.h
@@ -0,0 +1,21 @@
+#ifndef _C_COMMON_H
+#define _C_COMMON_H
+
+typedef int qboolean;
+
+static const int LOWER_LETTER_START = 97;
+static const int LOWER_LETTER_END = 122;
+
+static const int UPPER_LETTER_START = 65;
+static const int UPPER_LETTER_END = 90;
+
+static const int TRUE = 1;
+static const int FALSE = 0;
+
+static const int SIZE_END_SYMBOL = 1;
+
+static const int DIFFERENCE_LETTERS_SIZE = 32;
+
+static const char END_SYMBOL = '\0';
+
+#endif // !_C_COMMON_H
