e5ac44f168775e3383f3c6ced1db9428530549ba
nwpm <fwxgkt@gmail.com>
2025-05-07 14:47:38 +0300

    delete func _change_letter_size, rename _c_insert_char_sort to _c_sort_str_chars

--- src/internal/_str_internal.h
+++ src/internal/_str_internal.h
@@ -4,7 +4,6 @@
 #include "_c_common.h"
 
 // TODO: enum in _c_common.h?
-enum _operation_type { PLUS = 1, MINUS = 2 };
 
 int _c_count_tabs(char s[]);
 int _c_get_size_arr2d(const int rows, const int cols, char (*arr_words)[cols]);
@@ -19,12 +18,9 @@ void _c_get_arr_of_words(const int cols, char (*arr_words)[cols],
 void _c_get_words_arr_by_delim(const int cols, char (*arr_words)[cols],
                                const char s[], const char delim);
 void _c_swap_char(char *c1, char *c2);
-void _c_insert_char_sort(char s[]);
+void _c_sort_str_chars(char *s);
 void _c_init_char_arr2d(char init_by, const int rows, const int cols,
                         char (*arr_words)[cols]);
-void _c_change_letter_size(char s[], int bottom_border, int upper_border,
-                           enum _operation_type operation);
-
 void _c_insert_n_spaces(char s[], const int num_spaces, int *insert_from);
 
 int _c_is_string_of_spaces(char *s);
