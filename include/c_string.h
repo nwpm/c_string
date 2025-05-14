559426103e291bc95c92ddd4291ff718a6c8b4ea
nwpm <fwxgkt@gmail.com>
2025-05-14 20:35:28 +0300

    c_is_char_in_string change to c_index_of_char, c_delete_punctuation return value int now. c_strcpy, c_str_to_lower, c_str_to_upper, c_invert_symbols,c_reverse, c_change_char_all, c_delete_duplicates, c_sort_chars return value char*

--- include/c_string.h
+++ include/c_string.h
@@ -12,6 +12,8 @@ int c_strstr(const char *s, const char *substr);
 int c_num_substr(const char *s, const char *substr);
 int c_atoi(const char *s);
 int c_delete_spaces(char *s);
+int c_delete_punctuation(char *s);
+int c_index_of_char(const char *s, const unsigned char c);
 
 qboolean c_is_palindrom(const char *s);
 qboolean c_is_string_of_digits(const char *s);
@@ -20,7 +22,6 @@ qboolean c_is_letter(const unsigned char c);
 qboolean c_is_digit(const unsigned char c);
 qboolean c_is_space(const unsigned char c);
 qboolean c_is_empty_string(const char *s);
-qboolean c_is_char_in_string(const char *s, const unsigned char c);
 qboolean c_is_anagrams(const char *s1, const char *s2);
 qboolean c_is_upper_letter(const unsigned char c);
 qboolean c_is_lower_letter(const unsigned char c);
@@ -28,18 +29,18 @@ qboolean c_is_lower_letter(const unsigned char c);
 char c_letter_to_lower(unsigned char c);
 char c_letter_to_upper(unsigned char c);
 
-void c_str_to_lower(char *s);
-void c_str_to_upper(char *s);
-void c_invert_symbols(char *s);
-void c_reverse(char *s);
-void c_delete_punctuation(char *s);
+char *c_strcpy(char *to, const char *from);
+char *c_str_to_lower(char *s);
+char *c_str_to_upper(char *s);
+char *c_invert_symbols(char *s);
+char *c_reverse(char *s);
+char *c_change_char_all(char *s, const unsigned char from_char,
+                        const unsigned char to_char);
+char *c_delete_duplicates(char *s);
+char *c_sort_chars(char *s);
 
-void c_delete_duplicates(char *s);
-void c_strcpy(char *to, const char *from);
 void c_strcat(char *to, char *from);
-void c_change_char_all(char *s, const char from_char, const char to_char);
 void c_trim(char *s);
-void c_sort_chars(char *s);
 void c_detab(char *s, const int space_for_tab);
 void c_entab(char *s, const int space_for_tab);
 void c_insert_str_from(char *to, const char *substr, const int insert_from);
