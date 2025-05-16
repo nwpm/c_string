c5583008fa37364d28773b09e041ffd1d0f995bd
nwpm <fwxgkt@gmail.com>
2025-05-16 20:21:38 +0300

    added functions c_free_2d_array, c_count_words_space, c_count_words_delim, c_strcpy_safe, c_strcat_safe, c_insert_from, c_itoa_alloc, c_split_delim, c_split_space. deleted c_sort_chars, c_sort_words, c_str_replace, c_strtok

--- include/c_string.h
+++ include/c_string.h
@@ -3,6 +3,8 @@
 
 #include "../src/internal/_c_common.h"
 
+void c_free_2d_array(char **array, int size);
+
 int c_strlen(const char *s);
 int c_strcmp(const char *first, const char *second);
 int c_remove_digits(char *s);
@@ -13,6 +15,8 @@ int c_atoi(const char *s);
 int c_delete_spaces(char *s);
 int c_delete_punctuation(char *s);
 int c_index_of_char(const char *s, const unsigned char c);
+int c_count_words_space(const char *s);
+int c_count_words_delim(const char *s, unsigned char delim);
 
 qboolean c_is_palindrom(const char *s);
 qboolean c_is_string_of_digits(const char *s);
@@ -28,7 +32,8 @@ qboolean c_is_lower_letter(const unsigned char c);
 char c_letter_to_lower(unsigned char c);
 char c_letter_to_upper(unsigned char c);
 
-char *c_strcpy(char *to, const char *from);
+char *c_strcpy(char *dest, const char *from);
+char *c_strcpy_safe(char *dest, const char *from, const int dest_buff_size);
 char *c_str_to_lower(char *s);
 char *c_str_to_upper(char *s);
 char *c_invert_symbols(char *s);
@@ -36,25 +41,23 @@ char *c_reverse(char *s);
 char *c_change_char_all(char *s, const unsigned char from_char,
                         const unsigned char to_char);
 char *c_delete_duplicates(char *s);
-char *c_sort_chars(char *s);
 char *c_strcat(char *dest, const char *from, const int dest_buff_size);
+char *c_strcat_safe(const char *dest, const char *from,
+                    const int dest_buff_size);
 char *c_trim(char *s);
-char *c_detab(char *s, const int space_for_tab, const int buff_size);
-char *c_entab(char *s, const int space_for_tab, const int buff_size);
+char *c_detab(const char *s, const int space_for_tab);
+char *c_entab(const char *s, const int space_for_tab, const int buff_size);
 char *c_overwrite_from(char *dest, const char *substr, const int insert_from,
                        const int buff_size);
+char *c_insert_from(const char *dest, const char *substr, int insert_from);
 char *c_itoa(const int num, char *s);
+char *c_itoa_alloc(int num);
+char *c_delete_word_duplicate(char *s);
+
+char **c_split_delim(char *s, const char delim);
+char **c_split_space(char *s);
 
 // for correct work of these functions need alloc std functions
-int c_count_words(const char *s);
-void c_str_overwrite_first(char *s, const char *substr_old,
-                           const char *substr_new, const int buff_size);
 void c_reverse_word_order(char *s, const char delim);
-void c_delete_word_duplicate(char *s);
-void c_strtok(char *s, const int cols, char (*result)[cols], const char delim);
-void c_sort_words(char *s);
-void c_str_replace_all(char *s, const char *substr_old, const char *substr_new);
-void c_str_replace_n(char *s, const char *substr_old, const char *substr_new,
-                     const int n);
 
 #endif // !C_STRING_H
