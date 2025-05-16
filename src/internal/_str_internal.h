cdfca5da291bb3ee45d30a7d09460c72fe35eff0
nwpm <fwxgkt@gmail.com>
2025-05-16 20:24:09 +0300

    deleted _c_sort_chars and _c_sort_words

--- src/internal/_str_internal.h
+++ src/internal/_str_internal.h
@@ -3,8 +3,10 @@
 
 #include "_c_common.h"
 
-int _c_count_tabs(char s[]);
+int _c_count_tabs(const char *s);
 int _c_get_size_arr2d(const int rows, const int cols, char (*arr_words)[cols]);
+
+int _c_len_2d_array(char **array);
 
 void _c_delete_from_str(char s[], int expression_res);
 void _c_init_char_arr(char s[], char init_by, const int arr_size);
@@ -16,7 +18,6 @@ void _c_get_arr_of_words(const int cols, char (*arr_words)[cols],
 void _c_get_words_arr_by_delim(const int cols, char (*arr_words)[cols],
                                const char s[], const char delim);
 void _c_swap_char(char *c1, char *c2);
-char *_c_sort_str_chars(char *s);
 void _c_init_char_arr2d(char init_by, const int rows, const int cols,
                         char (*arr_words)[cols]);
 void _c_insert_n_spaces(char s[], const int num_spaces, int *insert_from);
@@ -25,8 +26,6 @@ int _c_is_string_of_spaces(char *s);
 
 void _c_delete_duplicate_str_arr2d(const int rows, const int cols,
                                    char (*arr_words)[cols]);
-
-void _sort_words_arr2d(const int rows, const int cols, char (*arr_words)[cols]);
 
 void _c_insert_str_from(char *s, const char *str, int *insert_from);
 
