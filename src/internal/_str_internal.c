78c7f1b53cc0a9565186f4f331b3b1ef6461bf01
nwpm <fwxgkt@gmail.com>
2025-05-18 19:43:38 +0300

    Removed internal, not used functions
    _str_internal
    Removed _c_count_tabs, c_count_spaces, _c_insert_n_spaces

--- src/internal/_str_internal.c
+++ src/internal/_str_internal.c
@@ -5,35 +5,3 @@ void _c_swap_char(char *c1, char *c2) {
   *c1 = *c2;
   *c2 = tmp_val;
 }
-
-int _c_count_tabs(const char *s) {
-
-  int number_of_tabs = 0;
-
-  for (int i = 0; s[i] != '\0'; ++i) {
-    if (s[i] == '\t') {
-      number_of_tabs++;
-    }
-  }
-
-  return number_of_tabs;
-}
-
-int _c_count_spaces(const char *s) {
-
-  int number_of_spaces = 0;
-
-  for (int i = 0; s[i] != '\0'; ++i) {
-    if (s[i] == ' ') {
-      number_of_spaces++;
-    }
-  }
-
-  return number_of_spaces;
-}
-
-void _c_insert_n_spaces(char *s, const int num_spaces, int *insert_from) {
-  for (int k = num_spaces; k != 0; --k) {
-    s[(*insert_from)++] = ' ';
-  }
-}
--- src/internal/_str_internal.h
+++ src/internal/_str_internal.h
@@ -3,10 +3,6 @@
 
 #include "_c_common.h"
 
-int _c_count_tabs(const char *s);
-int _c_count_spaces(const char *s);
-
 void _c_swap_char(char *c1, char *c2);
-void _c_insert_n_spaces(char s[], const int num_spaces, int *insert_from);
 
 #endif // !_STR_INTERNAL_H
